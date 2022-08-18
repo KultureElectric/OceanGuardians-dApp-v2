e16d4516c967950e5c16b0b8074be7142e2d9fd8
ruben <rubenusshd@gmail.com>
2022-08-18 11:27:39 +0200

    footer and favicon updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="shortcut icon" href="/favicon.ico"/>
+        <link rel="shortcut icon" href="/OG_Favicon.png"/>
         </Head>
         <body>
           <Main />
